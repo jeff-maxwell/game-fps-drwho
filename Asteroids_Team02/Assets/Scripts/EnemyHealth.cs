d39e69b8fb9d80b198d9ea4a534d214ad282b054
Van DeSantis <vandesantis98@gmail.com>
2019-04-13 15:19:43 -0500

    Create playershoot and enemyhealth scripts

---
+++ Asteroids_Team02/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int startingHealth = 3;
+    public int currentHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        // Set current health when enemy spawns
+        currentHealth = startingHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void TakeDamage()
+    {
+        currentHealth--;
+        if (currentHealth <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
