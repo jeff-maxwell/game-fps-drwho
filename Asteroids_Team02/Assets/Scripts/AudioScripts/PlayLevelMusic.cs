a4684adb062867d098d3aa530d538c2b1bcfdd92
Centupli <36423761+Centupli@users.noreply.github.com>
2019-04-15 10:04:30 -0500

    Added music when the level starts

---
+++ Asteroids_Team02/Assets/Scripts/AudioScripts/PlayLevelMusic.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayLevelMusic : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.GetComponent<AudioSource>().playOnAwake = false;
+        this.GetComponent<AudioSource>().loop = true;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        this.GetComponent<AudioSource>().Play();
+    }
+}
--- README.md
+++ README.md
@@ -2,7 +2,13 @@
 
 Resources Used
 
+Objects
+
 1. Starfield Skybox - Unity Assets Store
 2. Moon Model - Turbo Squid
 3. Asteroid Model - Turbo Squid
 4. Walls and Room Objects - Creepy Cat
+
+Sounds
+
+1. Creepy Background Music https://www.youtube.com/watch?v=GeEbOKKcW-0
