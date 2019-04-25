47e1c02c3445831d5c909d2b28707d725ae55014
jeff-maxwell <jeff@tcnsolutions.com>
2019-04-24 22:22:26 -0500

    Add Rotation script for Moon

---
+++ Asteroids_Team02/Assets/Scripts/Rotate.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    // public Variable that determines the planets rotation speed
+    public float rotationSpeed = 0.1f;
+
+    // Update called once per frame rotates the planets respectively according to their rotation speed.
+    void Update()
+    {
+        transform.Rotate(0, rotationSpeed, 0);
+    }
+}
