baeb1407818073e93396cb2918e6c2cf3559fba2
Jeff Maxwell <jeff@tcnsolutions.com>
2019-04-27 16:44:16 -0500

    Different Color Daleks

--- Asteroids_Team02/Assets/Scripts/LevelAI.cs
+++ Asteroids_Team02/Assets/Scripts/LevelAI.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class LevelAI : MonoBehaviour
 {
-    public GameObject dalek;
+    public GameObject dalek1;
+    public GameObject dalek2;
+    public GameObject dalek3;
+    public GameObject dalek4;
+    public GameObject dalek5;
     GameObject[] waypoints;
     int currentWP;
 
@@ -16,8 +20,31 @@ public class LevelAI : MonoBehaviour
     {   
         for (int x = 0; x < 5; x++)
         {
+            int dalekNumber = Random.Range(1, 5);
             currentWP = Random.Range(0, waypoints.Length - 1);
-            GameObject b = Instantiate(dalek, waypoints[currentWP].transform.position, waypoints[currentWP].transform.rotation);
+            GameObject b = new GameObject();
+
+            switch (dalekNumber)
+            {
+                case 1:
+                    b = Instantiate(dalek1, waypoints[currentWP].transform.position, waypoints[currentWP].transform.rotation);
+                    break;
+                case 2:
+                    b = Instantiate(dalek2, waypoints[currentWP].transform.position, waypoints[currentWP].transform.rotation);
+                    break;
+                case 3:
+                    b = Instantiate(dalek3, waypoints[currentWP].transform.position, waypoints[currentWP].transform.rotation);
+                    break;
+                case 4:
+                    b = Instantiate(dalek4, waypoints[currentWP].transform.position, waypoints[currentWP].transform.rotation);
+                    break;
+                case 5:
+                    b = Instantiate(dalek5, waypoints[currentWP].transform.position, waypoints[currentWP].transform.rotation);
+                    break;
+                default:
+                    b = Instantiate(dalek1, waypoints[currentWP].transform.position, waypoints[currentWP].transform.rotation);
+                    break;
+            }
         }
     }
 
