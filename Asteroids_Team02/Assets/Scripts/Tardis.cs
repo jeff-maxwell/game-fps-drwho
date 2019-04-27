4c660b674b02370fcb7fb3ac6d7be2320c79eaca
jeff-maxwell <jeff@tcnsolutions.com>
2019-04-27 11:50:03 -0500

    Added Tardis to teleport between levels

---
+++ Asteroids_Team02/Assets/Scripts/Tardis.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tardis : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject.tag);
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(2);
+        }
+    }
+}
