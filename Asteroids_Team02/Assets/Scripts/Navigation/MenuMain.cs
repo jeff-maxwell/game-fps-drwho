6f144ec194c303be2f41dd254768bd562bc7c7f8
Centupli <36423761+Centupli@users.noreply.github.com>
2019-04-27 16:55:47 -0500

    Main Menu Working

---
+++ Asteroids_Team02/Assets/Scripts/Navigation/MenuMain.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuMain : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+
+}
