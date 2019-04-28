171746ccca997bd9ea387d68516125d04d9760ee
Relck2012 <47149789+Relck2012@users.noreply.github.com>
2019-04-27 20:08:13 -0500

    Gun Animations and Partical System
    Added a scifi gun with recoil and partical system along with raycasts. If you play in my scene you will be able to walk around and the dalek will chase you. When you find him you can shoot and kill him upon three hits as what was set up previously by jeff. I still need to add the sound for the laser gun but every thing works.

---
+++ Asteroids_Team02/Assets/Scenes/Bret/Scripts/DalekController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DalekController : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+    NavMeshAgent dalek;
+
+    void Start()
+    {
+        dalek = this.GetComponent<NavMeshAgent>();
+        
+    }
+
+    private void Update()
+    {
+        SetDestination();
+    }
+
+    void SetDestination()
+    {
+        Vector3 target = player.transform.position;
+        dalek.SetDestination(target);
+    }
+}
---
+++ Asteroids_Team02/Assets/Scenes/Bret/Scripts/Gun.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gun : MonoBehaviour
+{
+    public float damage = 10f;
+    public float range = 1000f;
+    public float backfire = 10f;
+    public float recoil = 5f;
+
+    public Camera fpsCam;
+    public Image crossHair;
+    public GameObject firePoint;
+    public GameObject recoilPoint;
+    private GameObject effectToSpawn;
+    public List<GameObject> vfx = new List<GameObject>();
+
+    private float timeToFire = 0;
+    private Vector3 scale;
+    private Vector3 direction;
+    private Vector3 gunBeforeRecoil;
+    private Quaternion rotation;
+
+    private void Start()
+    {
+        effectToSpawn = vfx[0];
+        gunBeforeRecoil = transform.localPosition;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(0) && Time.time >= timeToFire)
+        {
+            timeToFire = Time.time + 1 / effectToSpawn.GetComponent<ProjectileMove>().fireRate;
+            Shoot();
+        }
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, gunBeforeRecoil, recoil * Time.deltaTime);
+        
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+
+        //Recoil
+        transform.localPosition = Vector3.MoveTowards(transform.localPosition, recoilPoint.transform.localPosition, backfire * Time.deltaTime);
+
+
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            RotateToHit(gameObject, hit.point);
+            SpawnVFX();
+            Target target = hit.transform.GetComponent<Target>();
+            if (target != null)
+            {
+                Debug.Log(hit.transform.name);
+                target.TakeDamage(damage);
+            }
+        }
+        else
+        {
+            rotation = fpsCam.transform.rotation;
+            SpawnVFX();
+        }
+
+
+    }
+
+    void RotateToHit(GameObject obj, Vector3 dest)
+    {
+        direction = dest - obj.transform.position;
+        rotation = Quaternion.LookRotation(direction);
+    }
+
+    void SpawnVFX()
+    {
+        GameObject vfx;
+
+        if (firePoint != null)
+        {
+            vfx = Instantiate(effectToSpawn, firePoint.transform.position, Quaternion.identity);
+            vfx.transform.rotation = rotation;
+        }
+        else
+        {
+            Debug.Log("No Fire Point");
+        }
+    }
+}
